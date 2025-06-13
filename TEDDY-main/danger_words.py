488469849ca60a83b122f5ea615f94305f953fae
Shalini <shalinisingh190603@gmail.com>
2025-06-13 12:33:02 +0530

    Initial commit - ZEAL Depression Bot

--- TEDDY-main/danger_words.py
+++
@@ -1,6 +0,0 @@
-def GetDangerWords():
-    words_file = open("concerning_words.txt")
-    danger_words = []
-    for word in words_file:
-        danger_words.append(word.rstrip())
-    return danger_words
